21fe0480a6314494209bf73e8496409c6a15e77f
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2023-12-22 19:27:14 -0300

    token config

--- frontend/src/pages/auth/protectedRoute/ProtectedRoute.jsx
+++ frontend/src/pages/auth/protectedRoute/ProtectedRoute.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ element: Element, ...rest }) => {
-  const token = localStorage.getItem('token');
-  // const token = null;
+  // const token = localStorage.getItem('token');
+  const token = null;
   const isAuthenticated = token !== null && token !== undefined && token !== "";
 
   return isAuthenticated ? <Element {...rest} /> : <Navigate to="/Welcome" />;
