fa54d0f63a98f1cb7148a131d7605ac46e0300d2
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-01-16 21:23:13 -0300

    Releasing access for deployment

--- frontend/src/pages/home/components/pagination/Pagination.jsx
+++ frontend/src/pages/home/components/pagination/Pagination.jsx
@@ -1,21 +1,16 @@
 // Pagination.jsx
-
 import React from 'react';
 
 export const Pagination = ({ currentPage, handlePreviousPage, handleNextPage, role }) => {
     return (
         <div className="page">
-            {/* {role === "ADMIN" && ( */}
-                {/* <> */}
-                    <button onClick={handlePreviousPage} disabled={currentPage === 0}>
-                        Back
-                    </button>
-                    <span>Página {currentPage + 1}</span>
-                    <button onClick={handleNextPage}>
-                        Next
-                    </button>
-                {/* </> */}
-            {/* )} */}
+            <button onClick={handlePreviousPage} disabled={currentPage === 0}>
+                Back
+            </button>
+            <span>Página {currentPage + 1}</span>
+            <button onClick={handleNextPage}>
+                Next
+            </button>
         </div>
     );
 };
